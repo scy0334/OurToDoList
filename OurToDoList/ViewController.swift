88eadb91cf0c244228fed1bd1b79c8d5aa5bdd45
Si Choi <0334pc@gmail.com>
2021-03-06 21:16:39 +0900

    Initial Commit

---
+++ OurToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurToDoList
+//
+//  Created by Si Choi on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
